5ec6a853ede7e78b7643d4bf561ef536f93f5d89
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-09-25 13:54:05 +0100

    saving tempory files of generated data that can be saved in cashe

--- grid_fire_count.r
+++ grid_fire_count.r
@@ -3,6 +3,7 @@ library(gitBasedProjects)
 library(snow)
 library(ncdf4)
 
+
 dir_nr = "data/fireCounts/"
 nlines_per_step = 10000
 
@@ -12,7 +13,7 @@ extent = extent(c(360-85, 360-30, -33, 15))
 
 processFile <- function(file, nlines_per_step, rv, lonlat2.5) {
     print(file)
-    
+    source("libs/filename.noPath.r")
     nlines = as.numeric(strsplit(system(paste('wc -l', file), TRUE), ' data')[[1]][1])
     cnames = colnames(read.csv(file, nrows = 1, skip = 0))
     
@@ -23,44 +24,52 @@ processFile <- function(file, nlines_per_step, rv, lonlat2.5) {
         if ((skip + nlines_per_step) > nlines) nrows = nlines - skip
         else nrows = nlines_per_step
         
-        dat   = read.csv(file, skip = skip, nrows = nrows, stringsAsFactors = FALSE)
+        temp_file = paste0('temp/', filename.noPath(file, noExtension = TRUE),
+                           '_lines-', nlines_per_step, '-', nlines, '-', skip, '-', nrows,
+                           '.Rd')
+        if (file.exists(temp_file)) load(temp_file)
+        else {
+            dat   = read.csv(file, skip = skip, nrows = nrows, stringsAsFactors = FALSE)
         
-        dates = strsplit(dat[, cnames == "datahora"], '/', fixed = TRUE)
-        mnths = as.numeric(sapply(dates, function(i) i[[2]]))
+            dates = strsplit(dat[, cnames == "datahora"], '/', fixed = TRUE)
+            mnths = as.numeric(sapply(dates, function(i) i[[2]]))
 
-        satalites =  dat[, cnames == "satelite"]
+            satalites =  dat[, cnames == "satelite"]
         
-        lons0 = lons = dat[, cnames == "longitude"]
-        lons[lons < 0] = 360 + lons[lons<0]
-        lons = round(lons/2.5)        
-        lats = round(dat[, cnames == "latitude" ]/2.5)
+            lons0 = lons = dat[, cnames == "longitude"]
+            lons[lons < 0] = 360 + lons[lons<0]
+            lons = round(lons/2.5)        
+            lats = round(dat[, cnames == "latitude" ]/2.5)
         
-        add2grid <- function(lon, lat, mnth, satalite, rv) {  
+            add2grid <- function(lon, lat, mnth, satalite, rv) {  
+                cell = which(lonlat2.5[,1] == lon & lonlat2.5[,2] == lat)
+                #layer = which(months == mnth)
             
-            cell = which(lonlat2.5[,1] == lon & lonlat2.5[,2] == lat)
-            #layer = which(months == mnth)
+                if (length(cell) == 0) return(rv)
+                else if (length(cell) > 1) {
+                    print("more than one")
+                }
             
-            if (length(cell) == 0) return(rv)
-            else if (length(cell) > 1) {
-                print("more than one")
+                item = which(names(rv) == satalite)
+            
+                if (length(item) == 0) {
+                    rnames = names(rv)
+                    rv[[length(rv)+1]] = rv[[1]]                
+                    names(rv) = c(rnames, satalite)
+                    item = length(rv)
+                }
+                rv[[item]][cell, mnth] = rv[[item]][cell, mnth] + 1            
+                return(rv)
             }
+        
+            for (i in 1:length(lons)) 
+                rv = add2grid(lons[i], lats[i], mnths[i], satalites[i], rv)
             
-            item = which(names(rv) == satalite)
-            
-            if (length(item) == 0) {
-                rnames = names(rv)
-                rv[[length(rv)+1]] = rv[[1]]                
-                names(rv) = c(rnames, satalite)
-                item = length(rv)
-            }
-            rv[[item]][cell, mnth] = rv[[item]][cell, mnth] + 1            
-            return(rv)
+            save(rv, file = temp_file)
         }
+            
+        gc()  
         
-        for (i in 1:length(lons)) 
-            rv = add2grid(lons[i], lats[i], mnths[i], satalites[i], rv)
-             
-        gc()
         return(rv)
     }
     rout = lapply(1:ceiling(nlines/nlines_per_step), readLines)
@@ -73,6 +82,7 @@ processFile <- function(file, nlines_per_step, rv, lonlat2.5) {
         index = which(sats == names(ri)[i])
         rv[[index]] = rv[[index]] + ri[[i]]
     } 
+    
     gc()
     return(rv)
 }
