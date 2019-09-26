5a61d9f58a68875c93e68a50c7c6d44863f83b2b
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-09-26 18:17:13 +0100

    adding required library

--- libs/writeRaster.Standard.r
+++ libs/writeRaster.Standard.r
@@ -1,4 +1,5 @@
 library(gitBasedProjects)
+library(ncdf4)
 
 writeRaster.Standard <- function(r, file, ...) 
     writeRaster.gitInfo(r, file, zname = 'time', zunit = 'month', overwrite = TRUE, ...)
