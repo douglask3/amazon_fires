a36ddb9a463226e3ab553fe83db484967758f96a
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-09-27 14:31:57 +0100

    copying layer names to switching pacific to regular centric !

--- libs/convert_pacific_centric_2_regular.r
+++ libs/convert_pacific_centric_2_regular.r
@@ -1,4 +1,5 @@
 convert_pacific_centric_2_regular <- function(dat, tempWrite = FALSE) {
+    dat_names = names(dat0)
     if (xmax(dat) < 180) return(dat)
 
     index = 1:length(values(dat[[1]]))
@@ -11,13 +12,15 @@ convert_pacific_centric_2_regular <- function(dat, tempWrite = FALSE) {
 
     xyz[,1] = x
     dat = rasterFromXYZ(xyz, crs = projection(dat))
+    names(dat) = dat_names
+
     if (tempWrite) dat = writeRaster(dat, file = memSafeFile())
     return(dat)
 }
 
 convert_regular_2_pacific_centric <- function(dat, tempWrite = FALSE) {
     if (xmax(dat) > 300) return(dat)
-    
+    dat_names = names(dat0)
     index = 1:length(values(dat[[1]]))
 
     xyz = cbind(xyFromCell(dat,index), values(dat))
@@ -29,6 +32,9 @@ convert_regular_2_pacific_centric <- function(dat, tempWrite = FALSE) {
 
     xyz[,1] = x
     dat = rasterFromXYZ(xyz, crs = projection(dat))
+
+    names(dat) = dat_names
+
     if (tempWrite) dat = writeRaster(dat, file = memSafeFile())
     return(dat)
 }
