f2961af1c09bfebda8ccd2b278e218234a7861d4
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-10-31 12:10:17 +0000

    tidying up and switching qs plot to number of years beeten

--- libs/plotStandardMap.r
+++ libs/plotStandardMap.r
@@ -6,6 +6,12 @@ library(plotrix)
 library(mapdata)
 library(mapplots)
 
+StandardLegend <- function(cols, limits, dat, rightx = 0.95, extend_max = TRUE, ...) 
+        add_raster_legend2(cols, limits, dat = dat, add = FALSE,
+                           transpose = FALSE, srt = 0, oneSideLabels= TRUE,
+                           plot_loc = c(0.01, rightx, 0.3, 0.78),
+                           ylabposScling = 1, extend_max = extend_max, ...)
+
 plotStandardMap <- function(r, cols, limits, e = NULL, add_legend = FALSE,
                             limits_error = c(0.05, 0.1),
                             title2 = '', title3 = '', ...) {
@@ -19,6 +25,8 @@ plotStandardMap <- function(r, cols, limits, e = NULL, add_legend = FALSE,
                             cols = cols, limits = limits, add_legend = FALSE,
                             quick = TRUE, ePatternRes = 5, ePatternThick = 0.67,
                             limits_error = limits_error, ...)
+                            
+    polygon(c(-62.5, -35, -35, -62.5), c(-56, -56, -50, -50), border = NA, col = "white")
     mtext(title3, adj = 0.1, line = 0.0)
     mtext(title2, side = 2, line = -1.5)
     if (add_legend) {
--- plot_firecount2019.r
+++ plot_firecount2019.r
@@ -13,8 +13,12 @@ qs =seq(0, 1, 0.1)
 cols_fc = c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026')
 limits_fc = c(0, 1, 10, 100, 200, 400, 600, 800)
 
-cols_pc = c('#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a')
-limits_pc = seq(0, 100, 10)
+cols_qs = c('#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a')
+limits_qs = 1:18
+
+cols_pc = c('#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d')
+cols_pc = rev(c('#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'))
+limits_pc = c(0, 1, 5, 10, 20, 40, 60, 80, 90, 95, 99, 100)
 
 grab_cache = TRUE
 
@@ -41,36 +45,36 @@ summeryFile <- function(file, addError = FALSE, tnameExt = 'asIs',  ...) {
     return(maps)
 }
 
+qrs <- function(rs)
+    sum(rs[[nlayers(rs)]] > rs[[-nlayers(obs)]])   
+
+mean.listedRasters <- function(rs) {
+    #browser()
+    r_mean = rs[[1]]
+    for (r in rs[-1]) r_mean = r_mean + r
+    r_mean = r_mean/length(rs) 
+    return(r_mean)
+}
+
+select_item <- function(i, rs = sims) layer.apply(rs, function(r) r[[i]])    
+
 obs = summeryFile(file_obs)
+obs_last = obs[[nlayers(obs)]]
+obs_mean = mean(obs)
+obs_maps = list(obs_mean, obs_last, qrs(obs))
+
 
 files_sim = list.files(dir_sim, full.names = TRUE)
 sims = lapply(files_sim, summeryFile, varname = "burnt_area")
-
-select_item <- function(i, rs = sims) layer.apply(rs, function(r) r[[i]])
-quantile.raster <- function(x) quantile(x, probs = qs, na.rm = FALSE)
-
-#simrs =  lapply(1:nlayers(obs), select_item)
-
-obs_last = obs[[nlayers(obs)]]
-obs_mean = mean(obs)
-
-obs_qs = obs
-obs_qs[is.na(obs_qs)] = 10E9
-obs_qs = raster::calc(obs_qs, quantile.raster)
-obs_qs[obs_qs > 9E9] = NaN
-obs_maps = list(obs_mean, obs_last, 100* sum(obs_last > obs_qs[[-1]])/(length(qs)-1))
-
-sim_mean = simrs[[1]]
-for (r in simrs[-1]) sim_mean = sim_mean + r#do.call(sum, simrs)
-sim_mean = sim_mean/length(simrs) 
+#simrs =  lapply(1:nlayers(obs), select_item
+sim_mean = mean.listedRasters(simrs)
 sim_last = tail(simrs, 1)[[1]]
+sim_qrs = layer.apply(sims, qrs)
+sim_qrs[[1]][is.na(obs[[1]])] = NaN
+sim_maps = list(sim_mean, sim_last, sim_qrs)
 
-sim_qs = lapply(sims, raster::calc, quantile.raster)
-sim_qs = layer.apply(1:length(sim_qs), function(i) 100* sum(sim_last[[i]] >= sim_qs[[i]][[-1]])/(length(qs)-1))
-sim_qs[sim_mean[[1]]>9E9] = NaN
-sim_maps = list(sim_mean, sim_last, sim_qs)
-cols = list(cols_fc, cols_fc,  cols_pc)
-limits = list(limits_fc, limits_fc, limits_pc)
+cols = list(cols_fc, cols_fc,  cols_qs)
+limits = list(limits_fc, limits_fc, limits_qs-0.5)
 graphics.off()
 pdf("figs/fireSeasonComaprison.pdf", height = 7, width = 5)#, res = 300, units = 'in')
     layout(rbind(c(1, 2, 2, 3), c(4, 5, 5, 6), c(7, 7, 8, 8), c(9, 10, 10, 11), 12),
@@ -79,40 +83,32 @@ pdf("figs/fireSeasonComaprison.pdf", height = 7, width = 5)#, res = 300, units =
     par(mar = mar, oma = c(0, 1.2, 1.2, 0))
     mapply(plotStandardMap, obs_maps, cols = cols, limits = limits,
            title2 = c("Observations", "", ""),
-           title3 = c("Average 2001-2019", "2019", "2019 Qunatile"))
+           title3 = c("Average 2001-2019", "2019", "No. years >"))
     mapply(plotStandardMap, sim_maps, cols = cols, limits = limits, 
            title2 = c("Simulated", "", ""))
 
-    StandardLegend <- function(cols, limits, dat, rightx = 0.99) 
-        add_raster_legend2(cols, limits, dat = dat, add = FALSE,
-                           transpose = FALSE, srt = 0, oneSideLabels= FALSE,
-                           plot_loc = c(0.01, rightx, 0.3, 0.7),
-                           ylabposScling = 1, extend_max = TRUE)
     par(mar = c(3, 0, 0, 0))                  
     StandardLegend(cols_fc, limits_fc, obs_maps[[1]], 0.9)
-    StandardLegend(cols_pc, limits_pc, obs_maps[[3]])
+    mtext('fire counts', side = 1, line = -2.5)
+    labelss = limits_qs
+    labelss[seq(1, length(labelss), 2)] = ''
+    StandardLegend(cols_qs, limits_qs, obs_maps[[3]],
+                   labelss = c('0', labelss),
+                   adj = 0,extend_max = FALSE)
+    mtext('No. years', side = 1, line = -2.5, adj = 0.75)
     par(mar = mar)
     sims_e = lapply(files_sim, summeryFile, varname = "burnt_area", addError = TRUE, tnameExt = 'plusError')
-    simrs_e =  lapply(1:nlayers(obs), select_item, sims_e)
-    #sims_e = sims
-    #simrs_e = simrs
-    ppoint = layer.apply(1:nlayers(obs), function(i) 100*mean(obs[[i]] > simrs_e[[i]]))
-    ppoint_maps = list(mean(ppoint), ppoint[[nlayers(ppoint)-1]], ppoint[[nlayers(ppoint)]])
-
-    cols = c('#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d')
+    #simrs_e =  lapply(1:nlayers(obs), select_item, sims_e)
+    
+    ppoint = layer.apply(1:nlayers(obs), function(i) 100.2*mean(obs[[i]] > simrs_e[[i]])-50.1)
+    ppoint_maps = list(mean(ppoint), ppoint[[nlayers(ppoint)]], ppoint[[nlayers(ppoint)-1]])
+    
     mapply(plotStandardMap, ppoint_maps,
            title2 = c("Probability of\nfire season", "", ""),
-           title3 = c("Average 2001-2019", "2018", "2019"),
-            MoreArgs = list(limits_error = c(0.25, 0.5), cols = cols, limits = limits_pc))
+           title3 = c("Average 2001-2019", "2019", "2018"),
+            MoreArgs = list(limits_error = c(0.25, 0.5), cols = cols_pc, limits = limits_pc-50))
 
     par(mar = c(3, 0, 0, 0)) 
-    StandardLegend(cols, limits_pc, obs_maps[[3]])
+    StandardLegend(cols_pc, limits_pc-50, ppoint_maps[[3]],#, labelss = c(0, limits_pc, 100)
+                   rightx = 0.9, extend_max = TRUE, extend_min = TRUE, units = '%')
 dev.off()
-
-cell = 300#
-
-plot(2001:2019, obs[cell], type = 'l', ylim = range(c(0, obs[cell])))
-for (sim in sims_e) 
-    lines(2001:2019, sim[cell], col = 'red')
-    
-lines(2001:2019, obs[cell], lty = 2)
