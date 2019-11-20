4435ecf9987a1bed8130a02fc4b5875dbdd3dad5
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-11-20 17:41:46 +0000

    running seasonal human ignitions

--- make_inputs/make_emc.r
+++ make_inputs/make_emc.r
@@ -4,6 +4,7 @@
 ## Libraries etc
 library(raster)
 library(rasterExtras)
+library(benchmarkMetrics)
 source("make_inputs/fuel_moisture_equilibrium.r")
 source("libs/writeRaster.Standard.r")
 
@@ -11,7 +12,7 @@ source("libs/writeRaster.Standard.r")
 files = paste0('data/', c('precip.mon.mean.nc', 'rhum.mon.mean.nc', 'air.mon.mean.nc'))
 mask_file = 'data/climate/climate_mask.nc'
 
-fname_out = 'outputs/climate/emc-'
+fname_outs = paste0('outputs/climate/',c('emc', 'seasonDiff'), '-')
 
 ################################################################################
 ## load data                                                                  ##
@@ -35,9 +36,32 @@ emc = layer.apply(1:nlayers(dat[[1]]), make_emc)
 mask = raster(mask_file)
 
 emc = raster::resample(emc, mask)
+
+################################################################################
+##   check height of dry season                                               ##
+################################################################################
+season = PolarConcentrationAndPhase(dat[[1]]*(-1), justPhase = TRUE, phase_units = 'months')
+season = raster::resample(season, emc)
+season_diff_from <- function(mn) {
+    diff = mn - season
+    test1 = diff > 6
+    test2 = diff < (-6)
+    diff[test1] = diff[test1] - 12
+    diff[test2] = diff[test2] + 12
+    
+    return(diff)
+}
+
+season_diff = layer.apply(1:12, season_diff_from)
+
 ################################################################################
 ## output                                                                     ##
 ################################################################################
+season_diff_out = layer.apply(1:nlayers(emc),
+                          function(mn) season_diff[[mn - 12*ceiling((mn-12)/12)]])
+
 years =  sapply(strsplit(names(dat[[1]]), '.', fixed = TRUE), substr, 2, 5)[1,]
-fname = paste0(fname_out, paste(range(years), collapse = '-'), '.nc')
-writeRaster.Standard(emc, fname)
+fname = paste0(fname_outs, paste(range(years), collapse = '-'), '.nc')
+writeRaster.Standard(emc, fname[1])
+writeRaster.Standard(season_diff_out, fname[2])
+
