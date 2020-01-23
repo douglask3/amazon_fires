31e98ab74840f04d9fd8f9ae1438b363ba0db9dd
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-01-23 17:37:32 +0000

    regions for all Australia (for optimization) and SE Aus (fort analysis)

--- grid2seAus.r
+++ grid2seAus.r
@@ -2,8 +2,8 @@ library(raster)
 source("libs/writeRaster.Standard.r")
 source("libs/convert_pacific_centric_2_regular.r")
 
-extent = extent(c(129, 155, -44, -21.25))
-extent = extent(c(112, 155, -44, -10))
+extents = list("SE_AUS_region" = extent(c(129, 155, -44, -21.25)),
+               "Australia_region" = extent(c(112, 155, -44, -10)))
 
 dirs = list.dirs('outputs/')
 dirs = dirs[!grepl('_region', dirs)]
@@ -11,20 +11,19 @@ dirs = dirs[!grepl('sampled_posterior_ConFire_solutions', dirs)]
 
 mask = 'data/climate/climate_mask.nc'
 
-
-regridFiles <- function(dir) {
+regridFiles <- function(dir, extent, rname) {
     files_in  = list.files(dir, full.names = TRUE)
     files_in  = files_in[grepl('.nc', files_in)]
 
     files_out = strsplit(files_in, '//', fixed = TRUE)
-    files_out = sapply(files_out, paste0, collapse = "/Australia_region/")
+    files_out = sapply(files_out, paste0, collapse = paste0("/", rname, "/"))
     
     regridFile <- function(file_in, file_out) { 
         print(file_in)
         print(file_out)
         
         r = r0 = brick(file_in)
-        if (extent(r)[2] == 144.5) {
+        if (extent(r)[2] == 144.5 || any(res(r) != res(mask_eg))) {
             extent(r) = extent(c(-1.25, 358.75, -90, 90))
             r = raster::resample(r, mask_eg)
         }
@@ -33,7 +32,10 @@ regridFiles <- function(dir) {
         
         rmask = all(is.na(r)) | any(r > 9E9) 
         if (grepl("lightning", file_in)) rmask = rmask | any(r < 0)
-        if (is.null(mask)) mask <<- rmask
+        if (is.null(mask)) {
+            browser()
+            mask <<- rmask
+        }
         else mask <<- mask | rmask
         writeRaster.Standard(r, file_out)
     }
@@ -41,15 +43,19 @@ regridFiles <- function(dir) {
     return(files_out)
 }
 
-mask_eg = raster(mask)
-mask = NULL
-files_out = unlist(sapply(dirs, regridFiles))
+regridRegion <- function(...) {
+    
+    mask  <<- NULL
+    files_out = unlist(sapply(dirs, regridFiles, ...))
 
-addMask <- function(file) {
-    print(file)
-    r = brick(file) 
-    r[mask] = NaN
-    writeRaster.Standard(r, file)
-}
+    addMask <- function(file) {
+        print(file)
+        r = brick(file) 
+        r[mask] = NaN
+        writeRaster.Standard(r, file)
+    }
 
-lapply(files_out, addMask)
+    lapply(files_out, addMask)
+}  
+mask_eg <<- raster(mask)     
+mapply(regridRegion, extents, names(extents))
