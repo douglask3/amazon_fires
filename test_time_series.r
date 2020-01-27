b08b826ea7b74fb29582eb5ba6f753742feae8b9
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-01-27 14:02:00 +0000

    plotting standard imitation and ROS (which will need its own axis) as well

--- test_time_series.r
+++ test_time_series.r
@@ -3,6 +3,7 @@ library(rasterExtras)
 source("libs/make_transparent.r")
 source("libs/plotStandardMap.r")
 source("libs/dev.off.gitInfo.r")
+source("libs/standardGrid.r")
 library(gitBasedProjects)
 graphics.off()
 layers = c(5, 95)
@@ -16,6 +17,12 @@ obs = "outputs/Australia_region/firecount-SE_Aus_2001_onwards.nc"
 
 liklihood = 'outputs/sampled_posterior_ConFire_solutions-RoSfirecount-Tnorm/constant_post_2018_full_2002_maxMaxT-MaxW/fire_summary_observed_liklihood.nc'
 
+controls_name = c("standard_fuel", "standard_moisture", "standard_ignitions",
+                  "standard_suppression", "variable")
+
+control_cols = make.transparent(c("#4d9221", "#053061", "#67001f", "#c51b7d", "black"), 0.5)
+control_cols[4] = make.transparent(control_cols[4], 0.5)
+control_dens = c(NA, 30, 30, NA, NA)
 
 mnths = 1:228
 
@@ -27,9 +34,9 @@ fire_seasons = mnths[seq(fire_season, max(mnths), by = 12)]
 temp_file_rs = paste0(c('temp/time_sries_fire_vars-ROS-TMAX-WMAX', layers), collapse = '-')
 temp_file = paste0(temp_file_rs, '-Open.Rd')
 
-fname = "figs/test_time_series-ROS-TNAX-WMAX.png"
+fname = "figs/test_time_series-ROS-TNAX-WMAX"
 
-grab.cache = FALSE
+grab.cache = TRUE
 
 openPost <- function(mnth, file, layer, ...) {
     print(mnth)
@@ -41,21 +48,22 @@ openPosts <- function(file, ...)
 
 
 regions = list('Gold Coast, Northern Rivers' = c(151.25, 153.75, -31.25, -26.75),
-               'Wollemi,  Blue Mountains NPs' = c(148.75, 151.25, -36.25, -31.25),
-               'Nadgee, Wallagaraugh River' = c(146.25, 148.75, -38.75, -36.25),
-               'Kangaroo Island, Adelaide' = c(136.25, 138.75, -36.25, -33.75))
+               'Wollemi,  Blue Mountains NPs' = c(148.75, 151.25, -36.25, -31.25))
 
 
 regions = list('SE Aus temperate BL woodland' = 'outputs/SE_TempBLRegion.nc',
                'Gold Coast, Northern Rivers' = c(151.25, 153.75, -31.25, -26.75),
-               'Wollemi,  Blue Mountains NPs' = c(148.75, 151.25, -36.25, -31.25))
+               'Wollemi,  Blue Mountains NPs' = c(148.75, 151.25, -36.25, -31.25),
+               'Nadgee, Wallagaraugh River' = c(146.25, 148.75, -38.75, -36.25),
+               'Kangaroo Island, Adelaide' = c(136.25, 138.75, -36.25, -33.75))
 
 if (file.exists(temp_file) && grab.cache) {
     load(temp_file) 
 } else {
     error = openPosts(error_file)
     uncert = openPosts(uncert_file, varname = "burnt_area")
-    save(error, uncert, file = temp_file)
+    controls = lapply(controls_name, function(i) openPosts(uncert_file, varname = i))
+    save(error, uncert, controls, file = temp_file)
 }
 
 obs = brick(obs)
@@ -88,50 +96,58 @@ cropMean <- function(r, extent, nme1, nme2, ...) {
     return(r)
 }
 
-polygonCoords <- function(x, y, col, border = col, ...) {
-    #y[y<0.00001]=0.00001
+polygonCoords <- function(x, y, col, border = col, maxY = NaN, ...) {
+    if (!is.na(maxY) && max(y) > maxY)
+        y = maxY * y/max(y)
+    
     polygon(c(x, rev(x)), c(y[,1], rev(y[,2])), col = col, border = border, ...)
 }
 
-plotRegion <- function(extent, name, last) {
+plotRegion <- function(extent, name, last, plot_control = FALSE) {
     tFile = paste(temp_file_rs, name, '.Rd')
-    if (file.exists(tFile) && grab.cache) {
+    if (file.exists(tFile) && FALSE) {
         load(tFile) 
     } else {
-        obs_r = cropMean(list(obs, obs), extent, name, 'obs')
-        obs_p = cropMean(list(p_value), extent, name, 'obs_p1')
-        obs_l = cropMean(list(liklihood), extent, name, 'obs_l')
-        error_r  = cropMean(error, extent, name, 'error')
-        uncert_r  = cropMean(uncert, extent, name, 'uncert')
+        obs_r      = cropMean(list(obs, obs), extent, name, 'obs')
+        obs_p      = cropMean(list(p_value), extent, name, 'obs_p1')
+        obs_l      = cropMean(list(liklihood), extent, name, 'obs_l')
+        error_r    = cropMean(error, extent, name, 'error')
+        uncert_r   = cropMean(uncert, extent, name, 'uncert')
+        
+        controls_r = mapply(cropMean, controls, nme2 = paste0(controls_name, '-CM'), 
+                            MoreArgs = list(extent = extent, nme1 = name), SIMPLIFY = FALSE)
+        save(obs_r, obs_p, obs_l, error_r, uncert_r, controls_r, file = tFile)
     }
-    
-    par(mar = c(1, 3, 0, 2))    
-    maxY = max(error_r, uncert_r, obs_r)
+
+    par(mar = c(1, 3, 0, 2))  
+    if (plot_control) maxY = 1.15  else maxY = max(error_r, uncert_r, obs_r)
     
     plot(c(12, max(mnths) + 2), c(0.0, maxY), xaxt = 'n', type = 'n', xaxs = 'i',
          xlab = '', ylab = '')
-    if (last)  axis(1, at = seq(1, length(mnths), 12),
-                    labels = seq(2001, length.out = floor(length(mnths)/12)))
+    standardGrid()
     mtext(name, adj = 0.1, line = -1.5)
    
-    polygonCoords(mnths, error_r, cols[1], lwd = 2)
-    polygonCoords(mnths, uncert_r, cols[2], lwd = 2)
-    polygonCoords(mnths, obs_r, cols[3], lwd = 2)
+    if (plot_control) {
+        mapply(polygonCoords, y = controls_r, col = control_cols, density = control_dens,
+               MoreArgs = list(x = mnths, lwd = 2, maxY = maxY))
+    } else {
+        polygonCoords(mnths, error_r, cols[1], lwd = 2)
+        polygonCoords(mnths, uncert_r, cols[2], lwd = 2)
+        polygonCoords(mnths, obs_r, cols[3], lwd = 2)    
 
-    labels = seq(0, 1, 0.2)
-    obs_l = (1 + obs_l)*maxY/2
-    
+        labels = seq(0, 1, 0.2)
+        obs_l = (1 + obs_l)*maxY/2
 
-    lines(mnths, obs_l, col = make.transparent("black", 0.67), lwd = 1.5)
-    #lines(mnths, obs_l, col = make.transparent("black", 1 - obs_p))
+        lines(mnths, obs_l, col = make.transparent("black", 0.67), lwd = 1.5)
+        #lines(mnths, obs_l, col = make.transparent("black", 1 - obs_p))
     
-    test2 = obs_p > 0.99
-    test1 = obs_p > 0.95
-    points(mnths[test1], obs_l[test1], pch = 19, cex = 2)
-    points(mnths[test2], obs_l[test2], pch = 4 , cex = 2)
-    axis(4, at = (1 + labels)*maxY/2, labels = labels)
-    
-    if (name == 'D') {
+        test2 = obs_p > 0.99
+        test1 = obs_p > 0.95
+        points(mnths[test1], obs_l[test1], pch = 19, cex = 2)
+        points(mnths[test2], obs_l[test2], pch = 4 , cex = 2)
+        axis(4, at = (1 + labels)*maxY/2, labels = labels)
+    }
+    if (name == tail(names(regions), 1)) {
          at = seq(1, max(mnths), by = 12)
         axis(1, at = at, labels = 2001 + round(at/12))
 
@@ -157,46 +173,57 @@ plotRegion <- function(extent, name, last) {
         }
         r
     }   
-    obs_r = climScale(obs_r+0.0001)
-    uncert_r = climScale(uncert_r+ 0.0001)
+    if (!plot_control) {
+        obs_r = climScale(obs_r+0.0001)
+        uncert_r = climScale(uncert_r+ 0.0001)
 
-    obs_r = obs_r[fire_seasons,]
-    uncert_r = uncert_r[fire_seasons,]
+        obs_r = obs_r[fire_seasons,]
+        uncert_r = uncert_r[fire_seasons,]
 
-    cols_years = make_col_vector(c("#161843", "#FFFF00", "#a50026"), ncols = 19)
-    par(mar = c(3, 0.5, 0, 3))  
-    plot(obs_r, uncert_r,
-         xlim = range(obs_r,uncert_r, obs_r*1.3), ylim = range(obs_r,uncert_r, obs_r*1.3),
-         xlab = '', ylab = '', yaxt = 'n', log = 'xy')
-    axis(4)
-    lines(x = c(0.0001, 9E9), y = c(0.0001,9E9), col = "black", lty = 2)
+        cols_years = make_col_vector(c("#161843", "#FFFF00", "#a50026"), ncols = 19)
+        par(mar = c(3, 0.5, 0, 3))  
+        plot(obs_r, uncert_r,
+            xlim = range(obs_r,uncert_r, obs_r*1.3), ylim = range(obs_r,uncert_r, obs_r*1.3),
+            xlab = '', ylab = '', yaxt = 'n', log = 'xy')
+        grid()
+        axis(4)
+        lines(x = c(0.0001, 9E9), y = c(0.0001,9E9), col = "black", lty = 2)
     
     
-    points(obs_r[,1], uncert_r[,1], pch = 19, col = cols_years)
-    points(obs_r[,2], uncert_r[,2], pch = 19, col = cols_years)
+        points(obs_r[,1], uncert_r[,1], pch = 19, col = cols_years)
+        points(obs_r[,2], uncert_r[,2], pch = 19, col = cols_years)
 
-    apply(cbind(obs_r[,], uncert_r[,]), 1,
-          function(i) lines(c(i[1], i[2]), c(i[3], i[4])))
+        apply(cbind(obs_r[,], uncert_r[,]), 1,
+            function(i) lines(c(i[1], i[2]), c(i[3], i[4])))
     
-    for (i in c(5, 8, 10, 19)) {
-        text(obs_r[i,1], mean(uncert_r[i,], 1), 2000 + i, srt = -90, adj = c(0.5, -0.5))
+        for (i in c(5, 8, 10, 19)) {
+            text(obs_r[i,1], mean(uncert_r[i,], 1), 2000 + i, srt = -90, adj = c(0.5, -0.5))
+        }
     }
 }
 
-png(fname, height = 7.3, width = 12, res = 300, units = 'in')
+ploFun <- function(fname, plot_control = FALSE, ...) {
+    png(fname, height = 12, width = 12, res = 300, units = 'in')
+        if (plot_control) lmat = rbind(t(matrix(rep(1:5, each = 2), nrow = 2)), 6)
+            else lmat = rbind(t(matrix(1:10, nrow = 2)), 11)
+        
+        layout(lmat, widths = c(.75, 0.25),
+               heights = c(1, 1, 1, 1, 1, 0.1))
+        par(oma = c(3, 1.2, 1, 1.2))
 
+        last = c(rep(FALSE, length(regions)-1), TRUE)
+        mapply(plotRegion, regions, names(regions), last,
+               MoreArgs = list(plot_control = plot_control, ...))
 
-layout(rbind(t(matrix(1:8, nrow = 2)), 9), widths = c(.75, 0.25), heights = c(1, 1, 1, 1, 0.1))
-par(oma = c(3, 1.2, 1, 1.2))
+        mtext.units(outer = TRUE, side = 2, 'Fire counts (k~m-2~)', line = -1)
+        mtext(outer = TRUE, side = 4, 'Modelled anomolie')
+        mtext(side = 1, 'Observed anomolie', line = 2.5)
 
-last = c(rep(FALSE, length(regions)-1), TRUE)
-mapply(plotRegion, regions, names(regions), last)
+        par(mar = rep(0, 4))
+        plot(0, axes = FALSE, type = 'n')
+    dev.off.gitWatermark()
+}
 
-mtext.units(outer = TRUE, side = 2, 'Fire counts (k~m-2~)', line = -1)
-mtext(outer = TRUE, side = 4, 'Modelled anomolie')
-mtext(side = 1, 'Observed anomolie', line = 2.5)
-
-par(mar = rep(0, 4))
-plot(0, axes = FALSE, type = 'n')
-dev.off.gitWatermark(comment = 'Note - meteorological input data for 2019/2020 fire season \nso recycled from 2018/2019')
+ploFun(paste0(fname, '.png'))
+ploFun(paste0(fname, '-controls.png'), plot_control = TRUE)
 
