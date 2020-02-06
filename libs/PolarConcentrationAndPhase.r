7d82a946bb5b81dd6e55f4e3eec02fb3b3968c76
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-02-06 17:30:14 +0000

    adding a function to test the modality of a fire season

--- libs/PolarConcentrationAndPhase.r
+++ libs/PolarConcentrationAndPhase.r
@@ -61,6 +61,33 @@ atans <- function(x, y, units = 'months', revs = FALSE) {
     }
 }
 
+testModal <- function(r) {
+    out0 = rep(NaN, 12)
+    cellModel <- function(y) {
+        if (any(is.na(y))) return(out0)
+        if (sum(y) == 0) return(out0)
+        
+        out0[1] = 1 - min(y)/max(y)
+        out0[7] = which.max(y)
+        testy = y[c(which.min(y):12, 1:which.min(y))]
+        dy = diff(testy)
+        peaks = which(testy > c(testy[-1], testy[1]) & testy >= c(tail(testy, 1), head(testy,-1)))
+        if (length(peaks) == 1) return(out0)
+        
+        peaks = peaks[sort.int(testy[peaks], index.return = TRUE)[[2]]]
+        out0[8:(6+length(peaks))] = head(peaks - tail(peaks,1),-1)
+        prominance <- function(p1, p2)
+            (testy[p1] - min(testy[p1:p2]))/max(testy)
+        
+        out = mapply(prominance, head(peaks, -1), peaks[-1])
+        out0[2:(length(out)+1)] = out
+        if (length(out) > 5) browser()
+        return(out0)
+    }
+    outr = r[[1:12]]
+    outr[] = t(apply(r[], 1,cellModel))
+    return(outr)
+}
 PolarConcentrationAndPhase.RasterBrick <- function(...)
     PolarConcentrationAndPhase.RasterStack(...)
 
