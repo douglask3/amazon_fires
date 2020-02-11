7fada40b0b45f0ea55416ae6b8ab02470898f25e
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-02-11 10:41:17 +0000

    adding state boundrarys. Needs shapefile. Might add later

--- libs/plotStandardMap.r
+++ libs/plotStandardMap.r
@@ -6,12 +6,21 @@ library(plotrix)
 library(mapdata)
 library(mapplots)
 
+aus_ste <- readOGR(dsn = "data/state_shape/STE11aAust.shp")
+
 StandardLegend <- function(cols, limits, dat, rightx = 0.95, extend_max = TRUE, oneSideLabels = TRUE, transpose = FALSE,
-                           plot_loc = c(0.01, rightx, 0.3, 0.56), srt = 0, add = FALSE, ...) 
-        add_raster_legend2(cols, limits, dat = dat, add = add,
-                           transpose = transpose, srt = srt, oneSideLabels= oneSideLabels,
-                           plot_loc = plot_loc,
-                           ylabposScling = 1, extend_max = extend_max, ...)
+                           plot_loc = NULL, srt = 0, add = FALSE, ...) {
+                           
+    if(is.null(plot_loc)){
+        if(add) plot_loc = c(0.02, 0.6, 0.1, 0.13)
+        else plot_loc = c(0.01, rightx, 0.3, 0.56)
+    }
+                           
+    add_raster_legend2(cols, limits, dat = dat, add = add,
+                       transpose = transpose, srt = srt, oneSideLabels= oneSideLabels,
+                       plot_loc = plot_loc,
+                       ylabposScling = 1, extend_max = extend_max, ...)
+}
 
 lineBox <- function(x, y, ...) 
     lines(c(x[1], x[2], x[2], x[1], x[1]), c(y[1], y[1], y[2], y[2], y[1]),...)
@@ -39,11 +48,11 @@ plotStandardMap <- function(r, cols, limits, e = NULL, add_legend = FALSE,
     plot(c(112, 155), c(-44, -10.5), xlab = '', ylab = '', axes = FALSE, type ='n', yaxs = 'i')
     grid()
     plot_raster_from_raster(r, e = e,
-                            cols = cols, limits = limits, add_legend = FALSE,
+                            cols = cols, limits = limits, add_legend = FALSE,interior = TRUE,
                             quick = TRUE, ePatternRes = 5, ePatternThick = 0.67,
                             limits_error = limits_error, add = TRUE, ...)
 
-    
+    lines(aus_ste)
     if (!is.null(regions))  lapply(regions, function(i) lineBox(i[1:2], i[3:4], lty = 2))
     
     polygon(c(-62.5, -35, -35, -62.5), c(-56, -56, -50, -50), border = NA, col = "white")
