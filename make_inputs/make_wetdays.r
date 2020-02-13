ff0a37f2a3294ac439cc168d6b9c365eb948c06a
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-02-13 09:08:24 +0000

    outputting precip from CPC instead

--- make_inputs/make_wetdays.r
+++ make_inputs/make_wetdays.r
@@ -12,7 +12,7 @@ dir = 'data/precip_daily/'
 mask_file = 'data/climate/climate_mask.nc'
 
 dir_out   = 'outputs/climate/'
-fname_out = 'wetdays-'
+fname_out = c('wetdays-', 'precip-')
 
 Start_yr = c(2001,1979)
 
@@ -23,32 +23,40 @@ subArea = extent(c(100, 170, -50, 0))
 ################################################################################
 
 files = list.files(dir, full.names = TRUE)
+
+findMnthWD <- function(mn, dat, mnths) {
+    print(mn)
+    mean(dat[[which(mn == mnths)]] >= 2.8)
+}
+
+findMnthPR <- function(mn, dat, mnths) {
+    print(mn)
+    mean(dat[[which(mn == mnths)]])
+}
+mask = raster(mask_file)
 makeData4Start_year <- function(syr) {
-    findWetdays <- function(file) {
+    findVar <- function(file, vFUN = findMnthWD) {
         dat = brick(file)
         dat = raster::crop(dat, subArea)
         mnths = sapply(names(dat), function(i) strsplit(i, '.', fixed = TRUE)[[1]][2])
-        findMnthWD <- function(mn) {
-            print(mn)
-            mean(dat[[which(mn == mnths)]] >= 2.8)
-        }
-        out = layer.apply(unique(mnths), findMnthWD)
+        
+        out = layer.apply(unique(mnths), vFUN, dat, mnths)
+        out = raster::resample(out, mask)
         return(out)
     }
     years = sapply(files, function(i) tail(strsplit(i, '.', fixed = TRUE)[[1]],2)[1])
     test = years >= syr
     files = files[test]; years = years[test]
 
-    wd = layer.apply(files, findWetdays)
-    
-   
-    mask = raster(mask_file)
-    wd = raster::resample(wd, mask)
+    wd = layer.apply(files, findVar)
+    pr = layer.apply(files, findVar, vFUN = findMnthPR)
+
     ################################################################################
     ## output                                                                     ##
     ################################################################################
     
-    writeInput(wd, dir_out, fname_out, years)
+    writeInput(wd, dir_out, fname_out[1], years)
+    writeInput(pr, dir_out, fname_out[2], years)
 }#
 
 lapply(Start_yr, makeData4Start_year)
