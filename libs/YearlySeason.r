8720387c9125a35f338974e23f3933a984ba18f3
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-02-13 16:21:45 +0000

    <= for condition to see if we can plot final years fire season, and allowing direct input on no. of months

--- libs/YearlySeason.r
+++ libs/YearlySeason.r
@@ -1,7 +1,9 @@
 YearlySeason <- function(season, r, FUN = 'sum') {
-    nyears = ceiling(nlayers(r)/12)
+    if (is.raster(r)) r = nlayers(r)
+    nyears = ceiling(r/12)
     seasons = lapply(1:nyears, function(i) season + (i-1) * 12)    
-    test = sapply(seasons, function(i) all(i < nlayers(r)))
+    test = sapply(seasons, function(i) all(i <= r))
+    
     seasons = seasons[test]
     if (is.null(FUN)) return(seasons)
     if (FUN == "sum") out = layer.apply(seasons, function(i) sum(r[[i]]))
