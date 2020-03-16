5afb490e6cf239a4d63f557cc9d560c417363e7e
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-03-16 08:49:04 +0000

    convering to line rather than plots and using annual average tree cover!

--- time_tree_cover_fire.r
+++ time_tree_cover_fire.r
@@ -1,22 +1,32 @@
+graphics.off()
 library(raster)
 
 fire = 'outputs/amazon_region/fire_counts/burnt_area_MCD64A1.006.nc'
 tree = 'outputs/amazon_region/vegetation/treecover-2001-June2018.nc'
-
-extent =  extent( -83.75, -28.75, -23.3, 0)
+agri = 'outputs/amazon_region/human/fract_agr2001-2019.nc'
+extent =  extent( -83.75, -28.75, -15, 0)
 
 fire = brick(fire)
-tree = brick(tree)
+tree = brick(tree)[[210]]
+agri = brick(agri)[[210]]
 
 fire = crop(fire, extent)
 tree = crop(tree, extent)
-treeLayer = nlayers(tree)
-tree = addLayer(tree, tree[[rep(210, 18)]])
+agri = crop(agri, extent)
+
+tree = tree/0.8
+tree = (tree + (1/2) * tree * agri/(1-tree-agri))
+
+tree[tree > 1] = 1
+tree = tree * 0.8
+#treeLayer = nlayers(tree)
+#tree = addLayer(tree, tree[[rep(210, 18)]])
 fireA = fire
 fireA[!is.na(fire)] = 1
 
 mask = raster('outputs/amazon_region/treeCoverTrendRegions.nc')
 mask = crop(mask, extent)
+mask[!(mask == 6 | mask == 3)]=NaN
 
 treeMean <- function(cov1, cov2) {
     print(c(cov1, cov2))
@@ -51,22 +61,29 @@ treeMean <- function(cov1, cov2) {
     }
     return(fireA)
 }
-covs = quantile(tree[], na.rm = TRUE, seq(0, 1, 0.1))
-for (i in 1:(length(covs)-1)) fireA =  treeMean(covs[i], covs[i+1])
+#covs = quantile(tree[], na.rm = TRUE, seq(0, 1, 0.1))
+#for (i in 1:(length(covs)-1)) fireA =  treeMean(covs[i], covs[i+1])
 
 ## covert fire to anomolies
-
+make_fire_anom <- function(mn) {
+    mn12 = mn - floor(mn/12)*12
+    if (mn12 == 0) mn12 = 12
+    return(fire[[mn]]/fire_clim[[mn12]])
+}
+fire_clim = layer.apply(1:12, function(mn) mean(fire[[seq(mn, nlayers(fire), by = 12)]]))
+fire_clim[fire_clim == 0]=1
+fireA = layer.apply(1:nlayers(fire), make_fire_anom)
 
 cols = c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026')
 limits = 1/(1+exp(-seq(-10, 0, 0.1)))
 
 dcols = rev(c('#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4'))
 
-dlimits = exp(seq(-1, 1, 0.02))
+dlimits = 50^(seq(-0.2, 0.2, 0.02))
 #dlimits =  c(-0.02, -0.01, -0.005, -0.002, -0.001, 0.001, 0.002, 0.005, 0.01, 0.02)
 
-cols = make.transparent(make_col_vector(cols, limits = limits), 0.95)
-dcols = make.transparent(make_col_vector(dcols, limits = dlimits+1), 0.95)
+cols = make.transparent(make_col_vector(cols, limits = limits), 0.8)
+dcols = make.transparent(make_col_vector(dcols, limits = dlimits+1), 0.8)
 
 tree_vs_fire_timeSlide <- function(i, fire, limits, cols) {
     
@@ -85,13 +102,38 @@ tree_vs_fire_timeSlide <- function(i, fire, limits, cols) {
     
 }
 
+tree_vs_fire_timeSlide2 <- function(i, fire, limits, cols, ...) {
+    if (is.na(mask[i])) return()
+    if (is.na(fire[[1]][i])) return()
+    x = 0:228
+    y = 100 -tree[i]*100
+    z = fire[i]
+    cols = cols[cut_results(z, limits)]
+    mapply(function(x1, x2, col) lines(c(x1, x2), c(y, y), col = col, ...),
+           head(x, -1), x[-1], cols)
+    
+    #lines(x, y, col = cols, ...)
+}
+
 doThePlot <- function(...) {
-    plot(c(1, 228), c(1, 100), pch = 19, cex = 1000, ylim = c(100, 17),
+    plot(c(0, 229), c(1, 100), pch = 19, cex = 1000, ylim = c(100, 17), log = 'y', 
         axes = FALSE, xaxs = 'i', yaxs = 'i')
-    for (i in 1:5) {
-        lapply(1:228, tree_vs_fire_timeSlide, ...)
-        lapply(228:1, tree_vs_fire_timeSlide, ...)
+    labels = c(80, 75, 70, 60, 40, 20, 0)
+       
+    axis(2, at = 100 - labels, labels = labels)
+    randomPlot <- function() {
+        
+        for (lwd in c(5, 3, 2, 1, 0.5, 0.1)) {
+            print(lwd)
+            index = sample(1:length(mask), size = round(length(mask)), replace = FALSE)
+            lapply(index, tree_vs_fire_timeSlide2, lwd = lwd, ...)
+        }
     }
+    for (nn in 1:3) randomPlot()
+    #for (i in 1:5) {
+    #    lapply(1:228, tree_vs_fire_timeSlide, ...)
+    #    lapply(228:1, tree_vs_fire_timeSlide, ...)
+    #}
 }
 png("figs/NewFig1Mabe.png", height = 150, width =183, units = 'mm', res = 300)
     par(mfrow = c(2, 1), mar = rep(0.5, 4), oma = c(2.5, 2.5, 0.5, 2.5))
